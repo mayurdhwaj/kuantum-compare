640cfb90b0f8bab0a11bc8bcf647760a2325eb84
Mayurdhwaj <mayurdhwajs94@gmail.com>
2023-08-18 15:56:48 +0530

    Added Main page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,18 @@
 import { type AppType } from "next/dist/shared/lib/utils";
-
 import "~/styles/globals.css";
+import { Sidebar } from "~/components/Sidebar";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+
+  return (
+    <main className="h-screen overflow-scroll snap-y snap-mandatory scroll-smooth flex overflow-x-hidden">
+      <Sidebar />
+      <div className="text-xl flex flex-col h-screen text-center py-12 mx-auto w-full pl-80">
+        <Component {...pageProps} />
+      </div>
+    </main>
+
+  );
 };
 
 export default MyApp;
